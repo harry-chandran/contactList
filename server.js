df771c5c7dac1f77efb9f39587b9f3d29413eb64
harry-chandran <hariharasudhan.ck@gmail.com>
2015-02-25 00:38:22 +0530

    mongodb config done by harry

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist',['contactlist'])
 
 /*app.get('/',function(req,res){
 	res.send("this message is from server")
@@ -9,7 +11,7 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/contactList',function(req,res){
 	//res.send("I recived a get request");
-	person1 = {
+	/*person1 = {
 		name : "harry",
 		email : "harry@gmail.com",
 		number : "9698566594"
@@ -25,7 +27,12 @@ app.get('/contactList',function(req,res){
 		number : "9698566594"
 	}
 	var contactList = [person1,person2,person3];
-	res.json(contactList);
+	res.json(contactList);*/
+
+	db.contactlist.find(function(err,docs){
+		console.log(docs);
+		res.json(docs);
+	});
 });
 
 app.listen(3000);
