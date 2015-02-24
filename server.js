f94f0533652290924bb3f57e41a1eb1c0b8af5b1
harry-chandran <hariharasudhan.ck@gmail.com>
2015-02-24 16:42:07 +0530

    static link added in server.js for index.html

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req,res){
+/*app.get('/',function(req,res){
 	res.send("this message is from server")
-});
+});*/
+
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
 console.log('hurrry my server is running')
