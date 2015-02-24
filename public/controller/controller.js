6be1b98ee4a13be37acf9afc3f73abe3735d1e66
harry-chandran <hariharasudhan.ck@gmail.com>
2015-02-24 21:26:02 +0530

    i added $http get in controller for ajax and server side get method

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,23 +1,12 @@
 var myApp = angular.module("myApp",[]);
 
-myApp.controller("AppCtrl",function($scope){
+myApp.controller("AppCtrl",function($scope,$http){
 
-	person1 = {
-		name : "harry",
-		email : "harry@gmail.com",
-		number : "9698566594"
-	};
-	person2 = {
-		name : "harry1",
-		email : "harry@gmail.com",
-		number : "9698566594"
-	};
-	person3 = {
-		name : "harry2",
-		email : "harry@gmail.com",
-		number : "9698566594"
-	}
+$http.get('/contactList').success(function(response){
+	$scope.contactList = response;
+});
+	
 
-	$scope.contactList = [person1,person2,person3];
+	
 
 })
--- server.js
+++ server.js
@@ -7,5 +7,26 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactList',function(req,res){
+	//res.send("I recived a get request");
+	person1 = {
+		name : "harry",
+		email : "harry@gmail.com",
+		number : "9698566594"
+	};
+	person2 = {
+		name : "harry1",
+		email : "harry@gmail.com",
+		number : "9698566594"
+	};
+	person3 = {
+		name : "harry2",
+		email : "harry@gmail.com",
+		number : "9698566594"
+	}
+	var contactList = [person1,person2,person3];
+	res.json(contactList);
+});
+
 app.listen(3000);
 console.log('hurrry my server is running')
