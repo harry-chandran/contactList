36d89c072cedaa06d7f5bf56e0401dd535c73c1d
harry-chandran <hariharasudhan.ck@gmail.com>
2015-02-25 16:21:31 +0530

    edit delete clear function and methods added

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -2,11 +2,44 @@ var myApp = angular.module("myApp",[]);
 
 myApp.controller("AppCtrl",function($scope,$http){
 
-$http.get('/contactList').success(function(response){
-	$scope.contactList = response;
-});
-	
+var refresh = function(){
+	$http.get('/contactList').success(function(response){
+		$scope.contactList = response;
+		$scope.contact = "";
+	});
+}
 
-	
+refresh();
+
+$scope.addContact = function(){
+	$http.post('/contactList',$scope.contact).success(function(response){
+		console.log(response);
+		refresh();
+	});
+};	
+
+$scope.remove = function(id){
+	$http.delete('/contactList/'+id).success(function(response){
+		console.log(response);
+		refresh();
+	});
+}
+
+$scope.edit = function(id){
+	$http.get('/contactList/'+id).success(function(response){
+		//console.log(response);
+		$scope.contact = response;
+	});
+}
+
+$scope.update = function(){
+	$http.put('/contactList/'+$scope.contact._id,$scope.contact).success(function(response){
+		//console.log(response);
+		refresh();
+	});
+}
+$scope.deselect = function(){
+	$scope.contact = "";
+}
 
 })
--- server.js
+++ server.js
@@ -3,11 +3,17 @@ var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('contactlist',['contactlist'])
 
+var bodyParser = require('body-parser');
+
+
+
 /*app.get('/',function(req,res){
 	res.send("this message is from server")
 });*/
 
+
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/contactList',function(req,res){
 	//res.send("I recived a get request");
@@ -30,10 +36,40 @@ app.get('/contactList',function(req,res){
 	res.json(contactList);*/
 
 	db.contactlist.find(function(err,docs){
-		console.log(docs);
+		//console.log(docs);
 		res.json(docs);
 	});
 });
 
+app.post('/contactList',function(req,res){
+	db.contactlist.insert(req.body,function(err,docs){
+		res.json(docs)
+	})
+});
+app.delete('/contactList/:id',function(req,res){
+	var id = req.params.id;
+	console.log(id)
+	console.log(mongojs.ObjectId(id))
+	db.contactlist.remove({_id:mongojs.ObjectId(id)},function(err,docs){
+		res.json(docs);
+	})
+})
+app.get('/contactList/:id',function(req,res){
+
+	var id = req.params.id;
+	db.contactlist.findOne({_id:mongojs.ObjectId(id)},function(err,docs){
+		res.json(docs);
+	});
+});
+
+app.put('/contactList/:id',function(req,res){
+
+	var id = req.params.id;
+	db.contactlist.findAndModify({query:{_id:mongojs.ObjectId(id)},
+		update:{$set:{name:req.body.name,email:req.body.email,number:req.body.number}},
+		new:true},function(err,docs){
+			res.json(docs);
+			});
+});
 app.listen(3000);
 console.log('hurrry my server is running')
